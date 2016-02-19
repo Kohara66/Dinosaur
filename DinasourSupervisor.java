3a324882f2556af56998d816f6a5428122730f15
Happy <happysibande@gmail.com>
2016-02-18 18:05:13 -0600

    begging of Dinasour program

---
+++ DinasourSupervisor.java
@@ -0,0 +1,14 @@
+package com.happy;
+
+public class DinasourSupervisor {
+
+    public static void main(String[] args) {
+
+        Dinosaur t_rex = new Dinosaur();
+        Dinosaur velociraptor = new Dinosaur();
+        t_rex.roar();
+        velociraptor.roar();
+
+
+    }
+}
---
+++ Dinosaur.java
@@ -0,0 +1,10 @@
+package com.happy;
+
+/**
+ * Created by Happy on 2/18/2016.
+ */
+public class Dinosaur {
+    public void roar(){
+        System.out.println("GRRR!!!!");
+    }
+}
